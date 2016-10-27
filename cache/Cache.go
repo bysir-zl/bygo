cb5fca745b67d0a7e8ed98b6d136483fc5d0fea8
bysir <1019654929@qq.com>
2016-10-27 18:23:25 +0800

    change cache config model; fix bug, DataUitl.objToMap by tag if obj field's tag is not defined, also add to the map .

--- cache/Cache.go
+++ cache/Cache.go
@@ -1,10 +1,12 @@
 package cache
 
+import "github.com/bysir-zl/bygo/config"
+
 func NewCache(driverType string) (CacheInterface) {
     switch driverType {
     case "redis":
-        return NewCacheRedis()
+        return NewCacheRedis(config.BConfig.RedisHost)
         break;
     }
-    return NewCacheRedis()
+    return NewCacheRedis(config.BConfig.RedisHost)
 }
