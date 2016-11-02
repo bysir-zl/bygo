14e8474c7267a5845d41d9075ce07ec704d2a0db
bysir <1019654929@qq.com>
2016-11-02 18:41:11 +0800

    util.Md5  add toString

--- util/Md5.go
+++ util/Md5.go
@@ -1,14 +1,21 @@
 package util
 
 import (
-    "crypto/md5"
-    "encoding/hex"
+	"crypto/md5"
+	"encoding/hex"
 )
 
-func Md5(in []byte) (out string) {
-    h := md5.New();
-    h.Write(in);
-    out = hex.EncodeToString(h.Sum(nil))
+func Md5(in []byte) (out []byte) {
+	h := md5.New();
+	h.Write(in);
+	mbs := h.Sum(nil)
+	out := make([]byte, hex.EncodedLen(len(mbs)))
+	hex.Encode(out, mbs)
 
-    return
+	return
+}
+func Md5String(in string) (out string) {
+	out := string(Md5([]byte(in)))
+	return
+
 }
