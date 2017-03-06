6dacf0a9e674e75fcc29ba13dfc5fa3f249ac5d6
bysir <1019654929@qq.com>
2017-03-06 13:56:09 +0800

    optimize log

--- log/log.go
+++ log/log.go
@@ -16,35 +16,43 @@ const (
 	LAssert
 )
 
-var maxLevel = 0
-var callDepth = 3
-
-var Logger *log.Logger
-
-func Verbose(tag string, v ...interface{}) {
-	OutPutStd(callDepth,LVerbose, tag, v...)
+type Logger struct {
+	l         *log.Logger
+	callDepth int
+	maxLevel  int
 }
 
-func Debug(tag string, v ...interface{}) {
-	OutPutStd(callDepth,LDebug, tag, v...)
-}
-func Info(tag string, v ...interface{}) {
-	OutPutStd(callDepth,LInfo, tag, v...)
+var defLogger *Logger
+
+func NewLogger() *Logger {
+	return &Logger{
+		l:         log.New(os.Stderr, "", log.Ltime|log.Ldate|log.Lshortfile),
+		callDepth: 3,
+		maxLevel:  0,
+	}
 }
 
-func Warn(tag string, v ...interface{}) {
-	OutPutStd(callDepth,LWarn, tag, v...)
+func (p *Logger) Verbose(tag string, v ...interface{}) {
+	p.OutPutStd(LVerbose, tag, v...)
 }
-func Error(tag string, v ...interface{}) {
-	OutPutStd(callDepth,LError, tag, v...)
+func (p *Logger) Debug(tag string, v ...interface{}) {
+	p.OutPutStd(LDebug, tag, v...)
+}
+func (p *Logger) Info(tag string, v ...interface{}) {
+	p.OutPutStd(LInfo, tag, v...)
+}
+func (p *Logger) Warn(tag string, v ...interface{}) {
+	p.OutPutStd(LWarn, tag, v...)
+}
+func (p *Logger) Error(tag string, v ...interface{}) {
+	p.OutPutStd(LError, tag, v...)
+}
+func (p *Logger) Assert(tag string, v ...interface{}) {
+	p.OutPutStd(LAssert, tag, v...)
 }
 
-func Assert(tag string, v ...interface{}) {
-	OutPutStd(callDepth,LAssert, tag, v...)
-}
-
-func OutPutStd(callDepth,level int, tag string, v ...interface{}) {
-	if maxLevel < level {
+func (p *Logger) OutPutStd(level int, tag string, v ...interface{}) {
+	if p.maxLevel < level {
 		l := ""
 		switch level {
 		case LVerbose:
@@ -61,38 +69,65 @@ func OutPutStd(callDepth,level int, tag string, v ...interface{}) {
 			l = "[A]"
 		}
 		tag = "[" + tag + "] "
-		Logger.SetPrefix(l+" ")
+		p.l.SetPrefix(l + " ")
 
 		var arg []interface{}
 		var format string
 
 		// 支持format
-		if len(v)>=2{
-			if _format,ok:=v[0].(string);ok{
-				if strings.Contains(_format,"%"){
+		if len(v) >= 2 {
+			if _format, ok := v[0].(string); ok {
+				if strings.Contains(_format, "%") {
 					format = _format
 					arg = v[1:]
 				}
 			}
 		}
 
-		if format==""{
-			arg =append([]interface{}{tag}, v...)
-			format =strings.Repeat("%+v ",len(arg))
+		if format == "" {
+			arg = append([]interface{}{tag}, v...)
+			format = strings.Repeat("%+v ", len(arg))
 		}
 
-		Logger.Output(callDepth, fmt.Sprintf(format,arg...))
+		p.l.Output(p.callDepth, fmt.Sprintf(format, arg...))
 	}
 }
 
+func (p *Logger) SetLogLevel(level int) {
+	p.maxLevel = level
+}
+
+func (p *Logger) SetCallDepth(d int) {
+	p.callDepth = d
+}
+
+func Verbose(tag string, v ...interface{}) {
+	defLogger.Verbose(tag, v...)
+}
+func Debug(tag string, v ...interface{}) {
+	defLogger.OutPutStd(LDebug, tag, v...)
+}
+func Info(tag string, v ...interface{}) {
+	defLogger.OutPutStd(LInfo, tag, v...)
+}
+func Warn(tag string, v ...interface{}) {
+	defLogger.OutPutStd(LWarn, tag, v...)
+}
+func Error(tag string, v ...interface{}) {
+	defLogger.OutPutStd(LError, tag, v...)
+}
+func Assert(tag string, v ...interface{}) {
+	defLogger.OutPutStd(LAssert, tag, v...)
+}
+
 func SetLogLevel(level int) {
-	maxLevel = level
+	defLogger.SetLogLevel(level)
 }
 
 func SetCallDepth(d int) {
-	callDepth = d
+	defLogger.SetLogLevel(d)
 }
 
 func init() {
-	Logger = log.New(os.Stderr, "", log.Ltime | log.Ldate | log.Lshortfile)
+	defLogger = NewLogger()
 }
