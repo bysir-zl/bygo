4685fcb8055fbc19d3b852966af6e6690fc0557c
bysir <zhangliang@kuaifazs.com>
2017-04-19 15:19:05 +0800

    n

--- util/random.go
+++ util/random.go
@@ -2,12 +2,13 @@ package util
 
 import (
 	"math/rand"
+	"time"
 )
 
 func Rand(min int, max int) (int) {
-	//sed := time.Now()
-	//time_int := sed.UnixNano()
-	//r := rand.New(rand.NewSource(time_int))
-	res_int := rand.Intn(max - min+1) + min
+	sed := time.Now()
+	time_int := sed.UnixNano()
+	r := rand.New(rand.NewSource(time_int))
+	res_int := r.Intn(max-min+1) + min
 	return res_int
 }
