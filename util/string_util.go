3fe34d0aabb7244c1fb18673230ee8bce4571354
bysir <zhangliang@kuaifazs.com>
2017-05-26 16:59:32 +0800

    add PackRsaKey

--- util/string_util.go
+++ util/string_util.go
@@ -5,11 +5,11 @@ func TuoFeng2SheXing(src []byte) (out []byte) {
 	out = []byte{}
 	for i := 0; i < l; i = i + 1 {
 		// 大写变小写
-		if 97 - 32 <= src[i] && src[i] <= 122 - 32 {
+		if 97-32 <= src[i] && src[i] <= 122-32 {
 			if (i != 0) {
 				out = append(out, 95)
 			}
-			out = append(out, src[i] + 32)
+			out = append(out, src[i]+32)
 		} else {
 			out = append(out, src[i])
 		}
@@ -34,28 +34,45 @@ func SheXing2TuoFeng(src []byte) (out []byte) {
 		// 是下划线
 		if 95 == src[i] {
 			// 下划线的下一个是小写字母
-			if 97 <= src[i + 1] && src[i + 1] <= 122 {
-				out[i - del] = src[i + 1] - 32
+			if 97 <= src[i+1] && src[i+1] <= 122 {
+				out[i-del] = src[i+1] - 32
 			} else {
-				out[i - del] = src[i + 1]
+				out[i-del] = src[i+1]
 			}
 			del++
 			i++
 		} else {
-			out[i - del] = src[i]
+			out[i-del] = src[i]
 		}
 	}
-	out = out[0 : l - del]
+	out = out[0: l-del]
 	return
 }
 
 func ChunkJoin(str, sub string, length int) string {
 	privateKey := ""
-	for i, l := 0, len(str) / length; i < l; i++ {
-		privateKey = privateKey + sub + str[i * length:(i + 1) * length]
+	for i, l := 0, len(str)/length; i < l; i++ {
+		privateKey = privateKey + sub + str[i*length:(i+1)*length]
+	}
+	if len(str)%length != 0 {
+		privateKey = privateKey + sub + str[len(str)-len(str)%length:]
 	}
 	if privateKey == "" {
 		return ""
 	}
-	return privateKey[1:]
+	return privateKey[len(sub):]
+}
+
+// 将一行的秘钥字符串转换为----BEGIN...多行的格式
+func PackRsaPrivateKey(s string) string {
+	return `-----BEGIN RSA PRIVATE KEY-----
+` + ChunkJoin(s, "\n", 64) + `
+-----END RSA PRIVATE KEY-----`
+}
+
+// 将一行的公钥字符串转换为----BEGIN...多行的格式
+func PackRsaPublicKey(s string) string {
+	return `-----BEGIN PUBLIC KEY-----
+` + ChunkJoin(s, "\n", 64) + `
+-----END PUBLIC KEY-----`
 }
