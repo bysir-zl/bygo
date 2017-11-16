2ec889cc563be38c50ce518ded116d36c82fb479
bysir-zl <1019654929@qq.com>
2017-11-16 23:21:43 +0800

    add config

---
+++ config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"github.com/go-ini/ini"
+	"os"
+)
+
+var f *ini.File
+
+func GetInt(key string, section string) (value int) {
+	value, _ = f.Section(section).Key(key).Int()
+	return
+}
+func GetString(key string, section string) (value string) {
+	value = f.Section(section).Key(key).String()
+	return
+}
+func GetBool(key string, section string) (value bool) {
+	value, _ = f.Section(section).Key(key).Bool()
+	return
+}
+
+// filePath "config/app.ini"
+func Load(filePath string) {
+	var err error
+	// 向上层查找文件
+	// 在项目的任何地方运行(test时)都能加载到配置文件
+	var file *os.File
+	for i := 0; i < 10; i++ {
+		f, err := os.Open(filePath)
+		if err != nil {
+			if os.IsNotExist(err) {
+				filePath = "../" + filePath
+			} else {
+				panic(err)
+			}
+		} else {
+			file = f
+			break
+		}
+	}
+	if file == nil {
+		panic("can't find config file")
+	}
+	f, err = ini.Load(file)
+	if err != nil {
+		panic(err)
+	}
+	f.BlockMode = false
+}
