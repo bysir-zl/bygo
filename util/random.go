3f8b9f506a27f82fec4f9c3163685718554d6fdd
zhangliang <zhangliang@zhuzi.me>
2017-12-05 14:37:44 +0800

    add ali sms

--- util/random.go
+++ util/random.go
@@ -3,12 +3,16 @@ package util
 import (
 	"math/rand"
 	"time"
+	"sync/atomic"
 )
 
+var i int64 = 0
+// 随机生成int型, 并发下++
 func Rand(min int, max int) (int) {
 	sed := time.Now()
-	time_int := sed.UnixNano()
-	r := rand.New(rand.NewSource(time_int))
-	res_int := r.Intn(max-min+1) + min
-	return res_int
+	timeInt := sed.UnixNano()
+	timeInt += atomic.AddInt64(&i, 1)
+	r := rand.New(rand.NewSource(timeInt))
+	resInt := r.Intn(max-min+1) + min
+	return resInt
 }
---
+++ util/sms/ali.go
@@ -0,0 +1,90 @@
+// 阿里的短信服务
+
+package sms
+
+import (
+	"github.com/bysir-zl/bygo/util"
+	"strconv"
+	"time"
+	"encoding/json"
+	"net/url"
+	"github.com/bysir-zl/bygo/util/encoder"
+	"crypto"
+	"github.com/bysir-zl/bygo/util/http_util"
+	"errors"
+	"strings"
+	"github.com/bysir-zl/bygo/log"
+)
+
+const (
+	HostAliSms = "http://dysmsapi.aliyuncs.com"
+	ApiAliSms  = ""
+)
+
+type Ali struct {
+	apiKey    string
+	apiSecret string
+}
+
+func NewAli(apiKey, apiSecret string) *Ali {
+	return &Ali{
+		apiKey:    apiKey,
+		apiSecret: apiSecret,
+	}
+}
+
+// 发送短信
+// phones 支持以逗号,分割的多个手机号码
+func (a *Ali) Send(tplCode string, signName string, phones string, data map[string]string) (error) {
+	bs, _ := json.Marshal(data)
+	l, _ := time.LoadLocation("GMT")
+	timestamp := time.Now().In(l).Format("2006-01-02T15:04:05Z")
+	params := map[string]string{
+		// 系统参数
+		"SignatureMethod":  "HMAC-SHA1",
+		"SignatureNonce":   strconv.Itoa(util.Rand(0, 9999999)),
+		"AccessKeyId":      a.apiKey,
+		"SignatureVersion": "1.0",
+		"Timestamp":        timestamp,
+		"Format":           "JSON",
+		// 业务API参数
+		"Action":        "SendSms",
+		"Version":       "2017-05-25",
+		"RegionId":      "cn-hangzhou",
+		"PhoneNumbers":  phones,
+		"SignName":      "阿里云短信测试专用",
+		"TemplateParam": string(bs),
+		"TemplateCode":  tplCode,
+		"OutId":         "123",
+	}
+	kv := util.ParseOrderKV(params)
+
+	signName, _ = a.Sign(kv)
+	kv.Add("Signature", signName)
+
+	//http.Get()
+
+	code, rsp, err := http_util.Get(HostAliSms+ApiAliSms, kv, nil)
+	if err != nil {
+		return err
+	}
+	if code != 200 {
+		return errors.New("rsp status is't 200, rsp:" + rsp)
+	}
+	if !strings.Contains(rsp, "OK") {
+		return errors.New("rsp :" + rsp)
+	}
+
+	return nil
+}
+
+// POP签名
+func (a *Ali) Sign(kv util.OrderKV) (sign string, err error) {
+	kv.Sort()
+	signStr := "GET" + "&" + url.QueryEscape("/") + "&" + url.QueryEscape(kv.EncodeString())
+	signBs := encoder.Hmac([]byte(signStr), []byte(a.apiSecret+"&"), crypto.SHA1)
+	signBs = encoder.Base64Encode(signBs)
+	sign = string(signBs)
+	log.Info("sing: ", sign)
+	return
+}
